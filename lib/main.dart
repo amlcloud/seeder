a14d342173fb8c4019e5c5448a54a994186cfdee
kovarny <sivochkin@gmail.com>
2022-06-27 18:27:14 +1000

    firestore support added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:seeder/people_page.dart';
 import 'package:seeder/sets_page.dart';
 import 'package:seeder/theme.dart';
+import 'firebase_options.dart';
 
 import 'dev_frame.dart';
 
-void main() {
-  runApp(TheApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(ProviderScope(child: TheApp()));
 }
 
 class TheApp extends StatelessWidget {
@@ -29,7 +39,7 @@ class TheApp extends StatelessWidget {
                       // print('onGenerateRoute: ${settings}');
                       if (settings.name == '/' || settings.name == 'entities') {
                         return PageRouteBuilder(
-                            pageBuilder: (_, __, ___) => PeoplePage());
+                            pageBuilder: (_, __, ___) => EntitiesPage());
                       } else if (settings.name == 'sets') {
                         return PageRouteBuilder(
                             pageBuilder: (_, __, ___) => SetsPage());
--- lib/people_page.dart
+++ lib/people_page.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:seeder/app_bar.dart';
+import 'package:seeder/providers/firestore.dart';
 
-class PeoplePage extends StatelessWidget {
-  const PeoplePage();
+class EntitiesPage extends ConsumerWidget {
+  const EntitiesPage();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: MyAppBar.getBar(context),
-      body: Container(
-          alignment: Alignment.center,
-          child: ListView(
-            padding: EdgeInsets.zero,
-            shrinkWrap: true,
-            children: [
-              ListTile(
-                title: Text('Test Widget 1'),
-              ),
-              ListTile(
-                title: Text('Text Widget 1'),
-              )
-            ],
-          )),
-    );
+        appBar: MyAppBar.getBar(context),
+        body: Container(
+            alignment: Alignment.center,
+            child: ListView(
+                padding: EdgeInsets.zero,
+                shrinkWrap: true,
+                children: ref.watch(colStreamProvider('entity')).when(
+                    loading: () => [Container()],
+                    error: (e, s) => [ErrorWidget(e)],
+                    data: (entities) => entities.docs
+                        .map((entity) => ListTile(
+                              title: Text('Entity ${entity.id}'),
+                            ))
+                        .toList()))));
   }
 }
