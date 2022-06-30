fd0f529cb8f7352f20d41f35b18e5741e1cc4516
VietTung <viettung0901@gmail.com>
2022-06-30 21:49:54 +1000

    Update entities_page.dart
    Added form to input entity value after pressing add button

--- lib/entities_page.dart
+++ lib/entities_page.dart
@@ -4,7 +4,7 @@ import 'package:seeder/app_bar.dart';
 import 'package:seeder/providers/firestore.dart';
 import 'package:seeder/widgets/entities_list.dart';
 import 'package:seeder/widgets/entity_details.dart';
-import 'package:seeder/widgets/entity_list_item.dart';                                // new
+import 'package:seeder/widgets/entity_list_item.dart'; // new
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class EntitiesPage extends ConsumerWidget {
@@ -23,19 +23,76 @@ class EntitiesPage extends ConsumerWidget {
                 children: [
                   Flexible(
                       child: Column(
-                    children: [EntitiesList(), buildAddEntityButton(ref)],
+                    children: [
+                      EntitiesList(),
+                      buildAddEntityButton(context, ref)
+                    ],
                   )),
                   Expanded(
                     child: EntityDetails('1'),
                   )
                 ])));
   }
+  // Code written by Joanne
+  // buildAddEntityButton(WidgetRef ref) {
+  //   return ElevatedButton(onPressed: () {
+  //     FirebaseFirestore.instance.collection('entity').add({'id':'', 'name':'', 'desc':''});
+  //   },
+  //   child: Text('Add Entity'));
+  // }
 
-  buildAddEntityButton(WidgetRef ref) {
-    return ElevatedButton(onPressed: () {
-      FirebaseFirestore.instance.collection('entity').add({'id':'', 'name':'', 'desc':''});
-    }, 
-    child: Text('Add Entity'));
+  // Edited version: vnguyen
+  buildAddEntityButton(BuildContext context, WidgetRef ref) {
+    TextEditingController id_inp = TextEditingController();
+    TextEditingController name_inp = TextEditingController();
+    TextEditingController desc_inp = TextEditingController();
+    return ElevatedButton(
+      child: Text("Add Entity"),
+      onPressed: () {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                scrollable: true,
+                title: Text('Adding Entity...'),
+                content: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Form(
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          controller: id_inp,
+                          decoration: InputDecoration(
+                            labelText: 'ID'
+                            ),
+                        ),
+                        TextFormField(
+                          controller: name_inp,
+                          decoration: InputDecoration(
+                            labelText: 'Name',
+                          ),
+                        ),
+                        TextFormField(
+                          controller: desc_inp,
+                          decoration: InputDecoration(
+                            labelText: 'Description',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                actions: [
+                  TextButton(
+                      child: Text("Submit"),
+                      onPressed: () {
+                        FirebaseFirestore.instance.collection('entity').add({'id': id_inp.text.toString(), 'name': name_inp.text.toString(), 'desc': desc_inp.text.toString()});
+                        Navigator.of(context).pop();
+                      })
+                ],
+              );
+            });
+      },
+    );
   }
-
 }
