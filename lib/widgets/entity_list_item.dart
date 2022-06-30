5e9acbe43b3fb1c5caffad5c67d70eebc15de4a7
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2022-06-30 10:19:15 +1000

    iteration for data of entity_list_item

--- lib/widgets/entity_list_item.dart
+++ lib/widgets/entity_list_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:seeder/providers/firestore.dart';
 
 class EntityListItem extends ConsumerWidget {
   final String entityId;
@@ -7,11 +8,22 @@ class EntityListItem extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Card(
-        child: ListTile(
-      title: Text('John Smith'),
-      trailing: Text('12345678'),
-      subtitle: Text('description...'),
-    ));
+    return ref.watch(docSP('entity/'+entityId)).when(
+        loading: () => Container(),
+        error: (e, s) => ErrorWidget(e),
+        data: (entityDoc) => entityDoc.exists == false
+            ? Center(child: Text('No entity data exists'))
+            :   Card(
+            child: Column(
+              children: [
+                ListTile(
+                  title: Text(entityDoc.data()!['name'] ?? 'name',),
+                  trailing: Text(entityDoc.data()!['id'] ?? 'id'),
+                  subtitle: Text(entityDoc.data()!['desc'] ?? 'desc'),
+                )
+              ],
+            )));
+
+
   }
 }
