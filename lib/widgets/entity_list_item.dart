a8367acc822fa20e526c7e65466d242dd344d03c
VietTung <viettung0901@gmail.com>
2022-06-30 21:29:36 +1000

    Update entity_list_item.dart
    Added alert dialog for deleting entity

--- lib/widgets/entity_list_item.dart
+++ lib/widgets/entity_list_item.dart
@@ -23,21 +23,38 @@ class EntityListItem extends ConsumerWidget {
                   trailing: Text(entityDoc.data()!['id'] ?? 'id'),
                   subtitle: Text(entityDoc.data()!['desc'] ?? 'desc'),
                 ),
-                buildDeleteEntityButton(ref, entityId)
+                buildDeleteEntityButton(context, ref, entityId)
               ],
             )));
 
 
   }
 
-  buildDeleteEntityButton(WidgetRef ref, id) {
+  buildDeleteEntityButton(BuildContext context, WidgetRef ref, id) {
     return ElevatedButton(onPressed: () {
-    
-    FirebaseFirestore.instance.runTransaction((Transaction myTransaction) async {
-      myTransaction.delete(FirebaseFirestore.instance.collection('entity').doc('/'+id));
-    });
-
-      
+      showDialog<String>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            title: const Text('Deleting entity'),
+            content: const Text('Are you sure you want to delete this entity?'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'Cancel'),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () { 
+                  Navigator.pop(context, 'OK');
+                  FirebaseFirestore.instance.runTransaction((Transaction myTransaction) async {
+                    myTransaction.delete(FirebaseFirestore.instance.collection('entity').doc('/'+id));
+                  });
+                  },
+                child: const Text('OK'),
+                
+              ),
+            ],
+          ),
+      ); 
     }, 
     child: Text('Delete Entity'));
   }
