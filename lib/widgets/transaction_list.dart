0d1a6340fa8011cceac9a5bf4100e41f81add645
kovarny <sivochkin@gmail.com>
2022-07-01 17:20:22 +1000

    transaction generation button

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:seeder/providers/firestore.dart';
+import 'package:seeder/widgets/transaction.dart';
+
+class TransactionList extends ConsumerWidget {
+  final String entityId;
+
+  const TransactionList(this.entityId);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) =>
+      ref.watch(colSP('entity/$entityId/transaction')).when(
+          loading: () => Container(),
+          error: (e, s) => ErrorWidget(e),
+          data: (trnCol) => trnCol.size == 0
+              ? Text('no records')
+              : Column(
+                  children: [
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: trnCol.docs.first
+                            .data()
+                            .entries
+                            .map((e) => Text(e.key.toString()))
+                            .toList()),
+                    ListView(
+                        padding: EdgeInsets.zero,
+                        shrinkWrap: true,
+                        children: trnCol.docs
+                            .map((trnDoc) => Transaction(trnDoc))
+                            .toList())
+                  ],
+                ));
+}
