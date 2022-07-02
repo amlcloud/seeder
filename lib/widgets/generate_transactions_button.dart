4f89d2017a3132243253c228ed9cdf05b73a426a
Snoresville <100341992+CalvinLe360@users.noreply.github.com>
2022-07-02 18:54:08 +0930

    Multiple transactions are now generated with button

--- lib/widgets/generate_transactions_button.dart
+++ lib/widgets/generate_transactions_button.dart
@@ -9,16 +9,19 @@ class GenerateTransactionsButton extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) => ElevatedButton(
       onPressed: () {
-        /// Generates a transaction of random amount and balance
-        FirebaseFirestore.instance
-            .collection('entity/${entityId}/transaction')
-            .add({
-          'amount': (math.Random().nextDouble() * 999 + 1).toStringAsFixed(2),
-          'balance': (math.Random().nextDouble() * 999 + 1).toStringAsFixed(2),
-          'ben_name': "Beneficiary",
-          'reference': "Example Transaction",
-          'rem_name': "Remitter",
-        });
+        /// Generates multiple transactions of random amount and balance
+        for (var i = 0; i < 10; i++){
+          FirebaseFirestore.instance
+              .collection('entity/${entityId}/transaction')
+              .add({
+            'amount': (math.Random().nextDouble() * 999 + 1).toStringAsFixed(2),
+            'balance':
+                (math.Random().nextDouble() * 999 + 1).toStringAsFixed(2),
+            'ben_name': "Beneficiary",
+            'reference': "Example Transaction",
+            'rem_name': "Remitter",
+          });
+        }
       },
       child: Text('Generate'));
 }
