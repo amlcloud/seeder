7edc74a7b3245405c03d3f8a82b4ca3365525e41
Snoresville <100341992+CalvinLe360@users.noreply.github.com>
2022-07-02 17:16:39 +0930

    Generate Transactions button functionality
    Generate button will now generate a transaction of random amount and balance

--- lib/widgets/generate_transactions_button.dart
+++ lib/widgets/generate_transactions_button.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'dart:math' as math;
 
 class GenerateTransactionsButton extends ConsumerWidget {
   final String entityId;
@@ -7,7 +9,16 @@ class GenerateTransactionsButton extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) => ElevatedButton(
       onPressed: () {
-        ///
+        /// Generates a transaction of random amount and balance
+        FirebaseFirestore.instance
+            .collection('entity/${entityId}/transaction')
+            .add({
+          'amount': (math.Random().nextDouble() * 999 + 1).toStringAsFixed(2),
+          'balance': (math.Random().nextDouble() * 999 + 1).toStringAsFixed(2),
+          'ben_name': "Beneficiary",
+          'reference': "Example Transaction",
+          'rem_name': "Remitter",
+        });
       },
       child: Text('Generate'));
 }
