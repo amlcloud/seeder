83af0c6ea4b812f16a5c18d61ca0897eac82ff69
VietTung <viettung0901@gmail.com>
2022-07-05 22:25:21 +1000

    fixed null exception in detail page
    Fixed returned null exception display on the entity detail page after deleting an entity

--- lib/widgets/entity_list_item.dart
+++ lib/widgets/entity_list_item.dart
@@ -11,58 +11,61 @@ class EntityListItem extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return ref.watch(docSP('entity/' + entityId)).when(
-        loading: () => Container(),
-        error: (e, s) => ErrorWidget(e),
-        data: (entityDoc) => entityDoc.exists == false
-            ? Center(child: Text('No entity data exists'))
-            : ListTile(
-              tileColor: Color.fromARGB(255, 44, 44, 44),
-              focusColor: Color.fromARGB(255, 133, 116, 116),
-                title: Text(
-                  entityDoc.data()!['name'] ?? 'name',
+          loading: () => Container(),
+          error: (e, s) => ErrorWidget(e),
+          data: (entityDoc) => entityDoc.exists == false
+              ? Center(child: Text('No entity data exists'))
+              : ListTile(
+                  tileColor: Color.fromARGB(255, 44, 44, 44),
+                  focusColor: Color.fromARGB(255, 133, 116, 116),
+                  title: Text(
+                    entityDoc.data()!['name'] ?? 'name',
+                  ),
+                  trailing: Column(children: <Widget>[
+                    Text(entityDoc.data()!['id'] ?? 'id'),
+                    buildDeleteEntityButton(context, ref, entityId)
+                  ]),
+                  subtitle: Text(entityDoc.data()!['desc'] ?? 'desc'),
+                  onTap: () {
+                    ref.read(activeEntity.notifier).value = entityId;
+                  },
                 ),
-                trailing: 
-                  Column(
-                    children: <Widget>[
-                    Text(entityDoc.data()!['id'] ?? 'id'),buildDeleteEntityButton(context, ref, entityId)]),
-                subtitle: Text(entityDoc.data()!['desc'] ?? 'desc'),
-                onTap: () {
-                  ref.read(activeEntity.notifier).value = entityId;
-                },
-              ),
-    );
+        );
   }
 
   buildDeleteEntityButton(BuildContext context, WidgetRef ref, id) {
     return IconButton(
-        onPressed: () {
-          showDialog<String>(
-            context: context,
-            builder: (BuildContext context) => AlertDialog(
-              title: const Text('Deleting entity'),
-              content:
-                  const Text('Are you sure you want to delete this entity?'),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () => Navigator.pop(context, 'Cancel'),
-                  child: const Text('Cancel'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context, 'OK');
-                    FirebaseFirestore.instance.runTransaction((Transaction myTransaction) async {
-                      myTransaction.delete(FirebaseFirestore.instance.collection('entity').doc(id));
-                    });
-                  },
-                  child: const Text('OK'),
-                ),
-              ],
-            ),
-          );
-        },
-        icon: Icon(Icons.delete),
-        padding: EdgeInsets.zero,
-        constraints: BoxConstraints(),
+      onPressed: () {
+        showDialog<String>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            title: const Text('Deleting entity'),
+            content: const Text('Are you sure you want to delete this entity?'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'Cancel'),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context, 'OK');
+                  FirebaseFirestore.instance
+                      .runTransaction((Transaction myTransaction) async {
+                    myTransaction.delete(FirebaseFirestore.instance
+                        .collection('entity')
+                        .doc(id));
+                  });
+                  ref.read(activeEntity.notifier).value = null;
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          ),
         );
+      },
+      icon: Icon(Icons.delete),
+      padding: EdgeInsets.zero,
+      constraints: BoxConstraints(),
+    );
   }
 }
