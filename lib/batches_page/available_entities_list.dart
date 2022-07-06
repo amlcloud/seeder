205b7004b9fb9d348594cb7bf413f8760f8d5e78
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-07-07 09:40:09 +1000

    store selected entity by existing firebase id

--- lib/batches_page/available_entities_list.dart
+++ lib/batches_page/available_entities_list.dart
@@ -8,31 +8,31 @@ import 'package:seeder/controls/doc_field_text_edit_delayed.dart';
 import 'package:seeder/providers/firestore.dart';
 
 class AvailableEntitiesList extends ConsumerWidget {
-  Future<bool> compareEntity(BuildContext context, WidgetRef ref, d) async {
-    bool tempreturn = true;
-    await FirebaseFirestore.instance
-        .collection('set')
-        .doc(ref.watch(activeBatch))
-        .collection('SelectedEntity')
-        .get()
-        .then((value) async => {
-              for (var snapshot in value.docs)
-                {
-                  await FirebaseFirestore.instance
-                      .collection('set')
-                      .doc(ref.watch(activeBatch))
-                      .collection('SelectedEntity')
-                      .doc(snapshot.id)
-                      .get()
-                      .then((value) => {
-                            if (value.data()!['entityId'].toString() ==
-                                d.toString())
-                              {tempreturn = false}
-                          })
-                }
-            });
-    return tempreturn;
-  }
+//   Future<bool> compareEntity(BuildContext context, WidgetRef ref, d) async {
+//     bool tempreturn = true;
+//     await FirebaseFirestore.instance
+//         .collection('set')
+//         .doc(ref.watch(activeBatch))
+//         .collection('SelectedEntity')
+//         .get()
+//         .then((value) async => {
+//               for (var snapshot in value.docs)
+//                 {
+//                   await FirebaseFirestore.instance
+//                       .collection('set')
+//                       .doc(ref.watch(activeBatch))
+//                       .collection('SelectedEntity')
+//                       .doc(snapshot.id)
+//                       .get()
+//                       .then((value) => {
+//                             if (value.data()!['entityId'].toString() ==
+//                                 d.toString())
+//                               {tempreturn = false}
+//                           })
+//                 }
+//             });
+//     return tempreturn;
+//   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) => ListView(
@@ -55,21 +55,40 @@ class AvailableEntitiesList extends ConsumerWidget {
               .toList())));
 
   fetchEntity(BuildContext context, WidgetRef ref, d) async {
-    if (await compareEntity(context, ref, d)) {
-      ref.watch(docSP('entity/' + d)).whenData((value) => {
+    // if (await compareEntity(context, ref, d)) {
+    //   ref.watch(docSP('entity/' + d)).whenData((value) => {
+    //         FirebaseFirestore.instance
+    //             .collection('set')
+    //             .doc(ref.read(activeBatch).toString())
+    //             .collection("SelectedEntity")
+    //             .doc(d.toString())
+    //             .set({
+    //           'id': value.data()!['id']?.toString(),
+    //           'author': value.data()!['author']?.toString(),
+    //           'desc': value.data()!['desc']?.toString(),
+    //           'name': value.data()!['name']?.toString(),
+    //           'time Created': value.data()!['time Created']?.toString(),
+    //         },SetOptions(merge: true)),
+    //       });
+    // }
+  // if(FirebaseFirestore.instance.collection('set').doc(ref.watch(activeBatch)).collection('SelectedEntity').doc(d).exists)
+   var docRef = FirebaseFirestore.instance.collection('set').doc(ref.watch(activeBatch)).collection('SelectedEntity').doc(d);
+   var doc = await docRef.get();
+   if (!doc.exists) {
+     ref.watch(docSP('entity/' + d)).whenData((value) => {
             FirebaseFirestore.instance
                 .collection('set')
                 .doc(ref.read(activeBatch).toString())
                 .collection("SelectedEntity")
-                .add({
+                .doc(d.toString())
+                .set({
               'id': value.data()!['id']?.toString(),
               'author': value.data()!['author']?.toString(),
               'desc': value.data()!['desc']?.toString(),
               'name': value.data()!['name']?.toString(),
               'time Created': value.data()!['time Created']?.toString(),
-              'entityId': d.toString()
-            }),
+            },SetOptions(merge: true)),
           });
-    }
+   }
   }
 }
