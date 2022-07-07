56648d8384d9ff0a067854547e3b84729f1d3bbd
ThuvaMathu <thuvamathu618@gmail.com>
2022-07-07 13:56:29 +1000

    shifte availabe entity to selected entity

--- lib/batches_page/available_entities_list.dart
+++ lib/batches_page/available_entities_list.dart
@@ -6,74 +6,87 @@ import 'package:seeder/batches_page/batch_entity_list_item.dart';
 import 'package:seeder/providers/firestore.dart';
 
 class AvailableEntitiesList extends ConsumerWidget {
-//   Future<bool> compareEntity(BuildContext context, WidgetRef ref, d) async {
-//     bool tempreturn = true;
-//     await FirebaseFirestore.instance
-//         .collection('set')
-//         .doc(ref.watch(activeBatch))
-//         .collection('SelectedEntity')
-//         .get()
-//         .then((value) async => {
-//               for (var snapshot in value.docs)
-//                 {
-//                   await FirebaseFirestore.instance
-//                       .collection('set')
-//                       .doc(ref.watch(activeBatch))
-//                       .collection('SelectedEntity')
-//                       .doc(snapshot.id)
-//                       .get()
-//                       .then((value) => {
-//                             if (value.data()!['entityId'].toString() ==
-//                                 d.toString())
-//                               {tempreturn = false}
-//                           })
-//                 }
-//             });
-//     return tempreturn;
-//   }
+  bool isexist(d, v) {
+    bool temp = true;
+    for (var i = 0; i < v.length; i++) {
+      if (v[i].id.toString() == d) {
+        print("foreach 111 id ${v[i].id.toString()} d: ${d}");
+        temp = false;
+        break;
+      }
+    }
+    return temp;
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) => ListView(
       padding: EdgeInsets.zero,
       shrinkWrap: true,
+      
+      // children: ref.watch(colSP('entity')).when(
+      //     loading: () => [Container()],
+      //     error: (e, s) => [ErrorWidget(e)],
+      //     data: (entities) => (entities.docs
+      //         .map((entity) =>  Card(
+      //               child: Row(children: [
+      //                 Expanded(
+      //                   child: BatchEntityListItem('entity/${entity.id}'),
+      //                 ),
+      //                 IconButton(
+      //                     onPressed: () => fetchEntity(context, ref, entity.id),
+      //                     icon: Icon(Icons.add)),
+      //               ]),
+      //             ))
+      //         .toList()))
+
       children: ref.watch(colSP('entity')).when(
           loading: () => [Container()],
           error: (e, s) => [ErrorWidget(e)],
-          data: (entities) => (entities.docs
-              .map((entity) => Card(
-                    child: Row(children: [
-                      Expanded(
-                        child: BatchEntityListItem('entity/${entity.id}'),
-                      ),
-                      IconButton(
-                          onPressed: () => fetchEntity(context, ref, entity.id),
-                          icon: Icon(Icons.add)),
-                    ]),
-                  ))
-              .toList())));
+          data: (entities) => ((ref
+              .watch(colSP('batch/${ref.watch(activeBatch)}/SelectedEntity/'))
+              .when(
+                  loading: () => [Container()],
+                  error: (e, s) => [ErrorWidget(e)],
+                  data: (value) => (entities.docs.map((entity) {
+                        if (isexist(entity.id.toString(), value.docs)) {
+                          return Card(
+                            child: Row(children: [
+                              Expanded(
+                                child:
+                                    BatchEntityListItem('entity/${entity.id}'),
+                              ),
+                              IconButton(
+                                  onPressed: () =>
+                                      fetchEntity(context, ref, entity.id),
+                                  icon: Icon(Icons.add)),
+                            ]),
+                          );
+                        } else {
+                          print("its me");
+                          return Container();
+                        }
+                      }).toList()))))));
+
+  fetchOnlyAvailable(BuildContext context, WidgetRef ref) async {
+    var docRef = FirebaseFirestore.instance
+        .collection('batch')
+        .doc(ref.watch(activeBatch))
+        .collection('SelectedEntity');
+    var doc = await docRef.get();
+    var entRef = FirebaseFirestore.instance.collection('wntity');
+    var ent = await entRef.get();
+    return ent.docs.where((item) => !doc.docs.contains(item)).toList();
+  }
 
   fetchEntity(BuildContext context, WidgetRef ref, d) async {
-    // if (await compareEntity(context, ref, d)) {
-    //   ref.watch(docSP('entity/' + d)).whenData((value) => {
-    //         FirebaseFirestore.instance
-    //             .collection('set')
-    //             .doc(ref.read(activeBatch).toString())
-    //             .collection("SelectedEntity")
-    //             .doc(d.toString())
-    //             .set({
-    //           'id': value.data()!['id']?.toString(),
-    //           'author': value.data()!['author']?.toString(),
-    //           'desc': value.data()!['desc']?.toString(),
-    //           'name': value.data()!['name']?.toString(),
-    //           'time Created': value.data()!['time Created']?.toString(),
-    //         },SetOptions(merge: true)),
-    //       });
-    // }
-  // if(FirebaseFirestore.instance.collection('set').doc(ref.watch(activeBatch)).collection('SelectedEntity').doc(d).exists)
-   var docRef = FirebaseFirestore.instance.collection('batch').doc(ref.watch(activeBatch)).collection('SelectedEntity').doc(d);
-   var doc = await docRef.get();
-   if (!doc.exists) {
-     ref.watch(docSP('entity/' + d)).whenData((value) => {
+    var docRef = FirebaseFirestore.instance
+        .collection('batch')
+        .doc(ref.watch(activeBatch))
+        .collection('SelectedEntity')
+        .doc(d);
+    var doc = await docRef.get();
+    if (!doc.exists) {
+      ref.watch(docSP('entity/' + d)).whenData((value) => {
             FirebaseFirestore.instance
                 .collection('batch')
                 .doc(ref.read(activeBatch).toString())
@@ -85,8 +98,8 @@ class AvailableEntitiesList extends ConsumerWidget {
               'desc': value.data()!['desc']?.toString(),
               'name': value.data()!['name']?.toString(),
               'time Created': value.data()!['time Created']?.toString(),
-            },SetOptions(merge: true)),
+            }, SetOptions(merge: true)),
           });
-   }
+    }
   }
 }
