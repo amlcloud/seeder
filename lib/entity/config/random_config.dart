8309ee012da45fde8b4081b248cfb94f6874111c
kovarny <sivochkin@gmail.com>
2022-07-19 09:42:39 +1000

    config ui change

--- lib/entity/config/random_config.dart
+++ lib/entity/config/random_config.dart
@@ -13,68 +13,38 @@ class PeriodicConfig extends StatelessWidget {
             border: Border.all(
               color: Colors.grey,
             )),
-        child: Column(
-          children: [
-            Expanded(child: Text('random trn')),
-            Row(mainAxisSize: MainAxisSize.max, children: [
-              Expanded(
-                  child: Column(
-                children: [
-                  Text('available periodic templates'),
-                  Column(
-                    children: [
-                      Card(
-                          child: ListTile(
-                        title: Text('monthly salary'),
-                        subtitle: Text('\$2000-\$10000'),
-                        trailing:
-                            IconButton(icon: Icon(Icons.add), onPressed: () {}),
-                      )),
-                      Card(
-                          child: ListTile(
-                        title: Text('weekly salary'),
-                        subtitle: Text('\$500-\$1000'),
-                        trailing:
-                            IconButton(icon: Icon(Icons.add), onPressed: () {}),
-                      )),
-                      IconButton(icon: Icon(Icons.add), onPressed: () {}),
-                    ],
-                  )
-                ],
-              )),
-              Expanded(
-                  child: Column(
-                children: [
-                  Text('selected periodic templates'),
-                  Column(
-                    children: [
-                      Card(
-                          child: ListTile(
-                        title: Text('Spotify subscription'),
-                        subtitle: Text('\$12.95, monthly on 15th'),
-                        trailing: IconButton(
-                            icon: Icon(Icons.close), onPressed: () {}),
-                      )),
-                      Card(
-                          child: ListTile(
-                        title: Text('gas bill'),
-                        subtitle: Text('~\$434, quarterly on 1st'),
-                        trailing: IconButton(
-                            icon: Icon(Icons.close), onPressed: () {}),
-                      )),
-                      Card(
-                          child: ListTile(
-                        title: Text('fortnightly salary'),
-                        subtitle: Text('~\$434, fortnightly on 11th'),
-                        trailing: IconButton(
-                            icon: Icon(Icons.close), onPressed: () {}),
-                      ))
-                    ],
-                  )
-                ],
-              )),
-            ])
-          ],
-        ));
+        child: Column(children: [
+          Expanded(child: Text('periodic trn')),
+          Expanded(
+              child: Column(children: [
+            Column(
+              children: [
+                Card(
+                    child: ListTile(
+                  leading: Switch(
+                      value:
+                          true, //ref.watch(isMineBatchNotifierProvider) ?? false,
+                      onChanged: (value) {
+                        //ref.read(isMineBatchNotifierProvider.notifier).value = value;
+                      }),
+                  title: Text('monthly salary'),
+                  subtitle: Slider(
+                    value: 10, //_currentSliderValue,
+                    max: 100,
+                    divisions: 5,
+                    // label: _currentSliderValue.round().toString(),
+                    onChanged: (double value) {
+                      // setState(() {
+                      //   _currentSliderValue = value;
+                      // });
+                    },
+                  ),
+                  trailing: IconButton(icon: Icon(Icons.add), onPressed: () {}),
+                )),
+                IconButton(icon: Icon(Icons.add), onPressed: () {}),
+              ],
+            )
+          ]))
+        ]));
   }
 }
