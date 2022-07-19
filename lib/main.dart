4d9e6e736074c867aa2ae5a5317bb3826b5bf207
dvaykhanskiy <dvaykhanskiy@ucvts.org>
2022-07-19 19:40:04 -0400

    test

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  //writing comment
 
   runApp(ProviderScope(
       child: MaterialApp(
