7afbb5297a27c85475264d8d64031ef35138891c
kovarny <sivochkin@gmail.com>
2022-07-21 10:25:34 +1000

    group

--- lib/widget_sandbox.dart
+++ lib/widget_sandbox.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:seeder/controls/group.dart';
 import 'package:seeder/theme.dart';
 
 import 'entity/create_recurrent_payment_dialog.dart';
@@ -26,7 +27,13 @@ void main() async {
           themeMode: ThemeMode.dark,
           theme: lightTheme,
           darkTheme: darkTheme,
-          home: Scaffold(body: EntityConfig('DcyMKCGiQ1ENnWaq7VVU')
+          home: Scaffold(
+              body: Group(
+            //       child: Column(
+            // children: [Text('hi'), Text('hi')],
+          ))
+
+              //EntityConfig('DcyMKCGiQ1ENnWaq7VVU')
 
               /// Insert your widget here for testing:
               //AddRecurrentPaymentDialog('DcyMKCGiQ1ENnWaq7VVU')
