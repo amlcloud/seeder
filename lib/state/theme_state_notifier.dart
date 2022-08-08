c9e137cd8e010f14ca6443b4f34d4b697b7be4af
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2022-08-08 21:44:58 +1000

    write back to db when theme change

--- lib/state/theme_state_notifier.dart
+++ lib/state/theme_state_notifier.dart
@@ -1,9 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ThemeStateNotifier extends StateNotifier<bool> {
-  ThemeStateNotifier(super.state);
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final dbInstance = FirebaseFirestore.instance;
+  ThemeStateNotifier(super.state) {}
   void changeTheme() {
     state = !state;
+    String themeMode = state == false ? 'light' : 'dark';
+    if (auth.currentUser != null) {
+      var userSettingDoc =
+          dbInstance.collection('user').doc(auth.currentUser!.uid);
+      userSettingDoc.set({'themeMode': themeMode});
+      // print(userSettingDoc);
+    }
   }
 }
 
