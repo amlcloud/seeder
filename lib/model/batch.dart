ba0ad12a268a36f85af4bb30b55acdd827e2a8f0
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2022-08-08 11:43:05 +1000

    batch mode

---
+++ lib/model/batch.dart
@@ -0,0 +1,13 @@
+class Batch {
+  String author;
+  String desc;
+  String id;
+  String name;
+  String timeCreated;
+  Batch(
+      {this.author = '',
+      this.desc = '',
+      this.id = '',
+      this.name = '',
+      this.timeCreated = ''});
+}
