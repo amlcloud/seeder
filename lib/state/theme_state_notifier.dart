55adb2360672a9b900e2a0ec1766c98a45c1f464
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2022-08-10 11:03:43 +1000

    Co-authored-by: kovarny <kovarny@users.noreply.github.com>

--- lib/state/theme_state_notifier.dart
+++ lib/state/theme_state_notifier.dart
@@ -1,12 +1,30 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:seeder/main.dart';
 
 class ThemeStateNotifier extends StateNotifier<bool> {
   final FirebaseAuth auth = FirebaseAuth.instance;
   final dbInstance = FirebaseFirestore.instance;
-  ThemeStateNotifier(super.state) {}
+  ThemeStateNotifier(bool loginState) : super(false) {
+    if (loginState == true && auth.currentUser != null) {
+      var userSettingDoc =
+          dbInstance.collection('user').doc(auth.currentUser!.uid);
+      Future theme = userSettingDoc.get().then((value) {
+        print('theme' + value.toString());
+        return value['themeMode'];
+      });
+      theme
+          .then((value) {
+            bool isDark = value == 'light' ? false : true;
+            return isDark;
+          })
+          .then((isDark) => state = isDark)
+          .whenComplete(() => print(state));
+    }
+  }
   void changeTheme() {
     state = !state;
     String themeMode = state == false ? 'light' : 'dark';
@@ -20,5 +38,7 @@ class ThemeStateNotifier extends StateNotifier<bool> {
 }
 
 final themeStateNotifierProvider =
-    StateNotifierProvider<ThemeStateNotifier, bool>(
-        (ref) => ThemeStateNotifier(false));
+    StateNotifierProvider<ThemeStateNotifier, bool>((ref) {
+  var loginState = ref.watch(isLoggedIn);
+  return ThemeStateNotifier(loginState);
+}, dependencies: [isLoggedIn]);
