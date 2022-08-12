fa291eeadbdafc3a35cf5ca908004ec3b9c99e81
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2022-08-12 10:27:35 +1000

    update toMap to batch data model

--- lib/model/batch.dart
+++ lib/model/batch.dart
@@ -1,13 +1,22 @@
 class Batch {
   String author;
-  String desc;
+  String description;
   String id;
   String name;
   String timeCreated;
   Batch(
       {this.author = '',
-      this.desc = '',
+      this.description = '',
       this.id = '',
       this.name = '',
       this.timeCreated = ''});
+  Map<String, dynamic> toMap() {
+    return {
+      'author': author,
+      'id': id,
+      'name': name,
+      'timecCreated': timeCreated,
+      'desc': description
+    };
+  }
 }
