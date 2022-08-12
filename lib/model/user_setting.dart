8bc5869fb876e9b2fd8bef697ecb15b93ca94c1f
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2022-08-12 10:41:38 +1000

    theme data model

---
+++ lib/model/user_setting.dart
@@ -0,0 +1,5 @@
+class UserSetting {
+  ThemeMode themeMode = ThemeMode.SYS;
+}
+
+enum ThemeMode { DARK, SYS, LIGHT }
--- lib/state/theme_state_notifier.dart
+++ lib/state/theme_state_notifier.dart
@@ -1,7 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:seeder/main.dart';
 
