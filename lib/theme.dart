be8b48546021c94109d24ae73fbe9615432cf0ab
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2022-08-16 10:42:12 +1000

    reset mannually dark/light theme setting to default

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,49 +2,9 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 
-final lightTheme = ThemeData(
-    // scaffoldBackgroundColor: Colors.black,
-    // primaryColor: Colors.white,
-    // backgroundColor: Color(0xff181a1b),
-    // colorScheme: const ColorScheme(
-    //     primary: Color(0xffcdcbc9),
-    //     secondary: Colors.grey,
-    //     onPrimary: Colors.blueGrey,
-    //     onSecondary: Color(0xff303035),
-    //     background: Color(0xff303035),
-    //     surface: Colors.black54,
-    //     onSurface: Color(0xffcdcbc9),
-    //     onBackground: Color(0xffcdcbc9),
-    //     error: Colors.red,
-    //     brightness: Brightness.dark,
-    //     onError: Color(0xffcdcbc9))
-    );
+final lightTheme = ThemeData.light();
 
-final darkTheme = ThemeData(
-    scaffoldBackgroundColor: Colors.black,
-    primaryColor: Colors.white,
-    backgroundColor: Color(0xff181a1b),
-    colorScheme: const ColorScheme(
-        primary: Color(0xffcdcbc9),
-        secondary: Colors.grey,
-        onPrimary: Colors.blueGrey,
-        onSecondary: Color(0xff303035),
-        background: Color(0xff303035),
-        surface: Colors.black54,
-        onSurface: Color(0xffcdcbc9),
-        onBackground: Color(0xffcdcbc9),
-        error: Colors.red,
-        brightness: Brightness.dark,
-        onError: Color(0xffcdcbc9)),
-    sliderTheme: ThemeData.dark().sliderTheme.copyWith(
-        valueIndicatorColor: Colors.black,
-        valueIndicatorTextStyle:
-            TextStyle(color: Colors.grey, backgroundColor: Colors.transparent)),
-    textTheme: TextTheme(
-      labelSmall: TextStyle(fontSize: 12, color: Colors.red),
-    ));
-
-
+final darkTheme = ThemeData.dark();
 
 //style static class for login_page
 class LoginStyle {
