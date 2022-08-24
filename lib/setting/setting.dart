4e4395f1818d49e8417d34fa2eed3493888a4211
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2022-08-24 21:24:43 +1000

    Create setting page (#91)
    1. fix theme change
    
    2. add setting content widget

---
+++ lib/setting/setting.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Setting extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
